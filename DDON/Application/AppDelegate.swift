38fb5b4ce2eb45ec995e42bc2129a20296e67d3e
pil_gaaang <21900204@handong.ac.kr>
2024-11-20 17:41:32 +0900

    feat/#1-file&view setting

---
+++ DDON/Application/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  DDON
+//
+//  Created by Pil_Gaaang on 11/20/24.
+//
+import SwiftUI
+
+import UIKit
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        print("AppDelegate initialized")
+        return true
+    }
+}
---
+++ DDON/Sources/Presentations/Camera/Camera View/CameraView.swift
@@ -0,0 +1,16 @@
+//
+//  CameraView.swift
+//  DDON
+//
+//  Created by Pil_Gaaang on 11/20/24.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        NavigationView {
+            Text("Camera Screen")
+        }
+    }
+}
